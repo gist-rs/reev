828657f6f4ede1cf8b9d1dbb4359c59b8fcdd4ef
katopz <katopz@gmail.com>
2025-09-24 16:53:51 +0900

    fix: test

--- crates/reev-lib/src/lib.rs
+++ crates/reev-lib/src/lib.rs
@@ -13,10 +13,10 @@ mod tests {
 
     #[test]
     fn it_loads_benchmark() {
-        let benchmark_file = "../../benchmarks/transfer-simple-001.yml";
+        let benchmark_file = "../../benchmarks/001-sol-transfer.yml";
         let f = std::fs::File::open(benchmark_file).unwrap();
         let test_case: TestCase = serde_yaml::from_reader(f).unwrap();
-        assert_eq!(test_case.id, "TRANSFER-SIMPLE-001");
+        assert_eq!(test_case.id, "001-SOL-TRANSFER");
         assert_eq!(test_case.initial_state.len(), 2);
     }
 }
